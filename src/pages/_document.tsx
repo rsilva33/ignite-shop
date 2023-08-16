edec5421a2a7be56e403bbdfd29944317a11e663
Ronaldo Silva <ronaldo.silva.jr96@gmail.com>
2023-08-16 14:29:26 -0300

    configuring stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { getCssText } from '@/styles'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 // todo elemento dentro desse arquivo sera carregado em toda aplicacao
@@ -6,8 +7,20 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        {/* Quando o usuario carregar a pagina, ve qualque que e o codigo necessario para pagina  */}
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
       </Head>
       <body>
         {/* serve para indicar para o next em qual lugar do html do documento da aplicacao vao os conteudos das paginas que sao carregadas */}
