dcf5c206fddba50be494032bf13f3ea0f3b6b041
Ronaldo Silva <ronaldo.silva.jr96@gmail.com>
2023-08-18 10:15:51 -0300

    home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,30 @@
-import { styled } from '../styles'
+import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/home'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
+import shirt1 from '../assets/shirts/shirt-1.png'
+import shirt2 from '../assets/shirts/shirt-2.png'
+import shirt3 from '../assets/shirts/shirt-3.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Testedassd</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
