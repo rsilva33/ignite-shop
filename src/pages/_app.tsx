b3d52e32caca7b677c6268d0f40a48d04c54194e
Ronaldo Silva <ronaldo.silva.jr96@gmail.com>
2023-08-18 08:51:43 -0300

    images in the next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { globalStyles } from '../styles/global'
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
 
+import Image from 'next/image'
+
 globalStyles()
 
 // Funciona como um container para as paginas da aplicacao, e carregado junto com qualquer pagina da aplicacao
@@ -11,7 +13,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
