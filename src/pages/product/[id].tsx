7e325241b37d3c221f02840884a1796ae53c2821
Ronaldo Silva <ronaldo.silva.jr96@gmail.com>
2023-10-20 18:26:16 -0300

    stripe checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -15,6 +15,7 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
@@ -24,6 +25,9 @@ export default function Product({ product }: ProductProps) {
   // if (isFallBack) {
   //   return <p></p>
   // }
+  function handleBuyButton() {
+    console.log(product.defaultPriceId)
+  }
 
   return (
     <ProductContainer>
@@ -37,7 +41,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Buy now</button>
+        <button onClick={handleBuyButton}>Buy now</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -76,6 +80,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: 'BRL',
         }).format(Number(price.unit_amount) / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1 hour
